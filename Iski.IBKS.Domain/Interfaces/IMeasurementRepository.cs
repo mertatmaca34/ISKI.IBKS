8506175f306145daecd356cfd264c5dfd54b001d
Mert Atmaca <mertatmaca34@gmail.com>
2025-11-26 17:52:36 +0300

    ++
    ++

---
+++ Iski.IBKS.Domain/Interfaces/IMeasurementRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISKI.IBKS.Domain.Interfaces
+{
+    internal class IMeasurementRepository
+    {
+    }
+}
